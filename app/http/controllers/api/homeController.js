5908a364f5fdde87e3cd86c1b93bc46d2560d20c
mohammad alamzade <mmmohammadalam@gmail.com>
2023-06-14 23:36:29 +0330

    commit day 2

--- app/http/controllers/api/homeController.js
+++ app/http/controllers/api/homeController.js
@@ -5,7 +5,7 @@ const controller = require("../controller");
 module.exports = new (class homeController extends controller {
   async indexPage(req, res, next) {
     try {
-      const result = await authSchema.validateAsync(req.body);
+      await authSchema.validateAsync(req.body);
       return res.status(200).send("index page store");
     } catch (err) {
       next(createError.BadRequest(err.message));
