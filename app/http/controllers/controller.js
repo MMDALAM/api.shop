a11d48915e9a6068965a23f46b94d9a4947ad763
mohammad alamzade <mmmohammadalam@gmail.com>
2023-07-14 00:39:44 +0330

    commit now

--- app/http/controllers/controller.js
+++ app/http/controllers/controller.js
@@ -1,5 +1,4 @@
 const autoBind = require("auto-bind-inheritance");
-
 module.exports = class controller {
   constructor() {
     autoBind(this);
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("app-module-path").addPath(__dirname);
 
 const Application = require("./app/server");
+
 new Application(5000, "mongodb://127.0.0.1:27017/testShopNodejs");
